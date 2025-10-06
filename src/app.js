7a40f4a1ba275e6263bafb409502aa8d9b9a154b
Adarsh Mishra <adarshmishra2@yahoo.in>
2025-10-07 01:56:25 +0530

    added mongodb database connection and refactored the index.js file

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+//basic configurations
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true, limit:"16kb"}));
+app.use(express.static("public"));
+
+//cors configurations
+app.use(cors({
+    origin:process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials:true,
+    methods:["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders:["Authorization","Content-Type"]
+}))
+
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js"
+import connectDB from "./db/index.js";
 
 dotenv.config({
     path:"./.env",
@@ -11,9 +12,15 @@ const port = process.env.PORT || 3000
 // app.get()
 
 
-app.listen(port, ()=>{
+connectDB()
+.then(()=>{
+    app.listen(port, ()=>{
     console.log(`Listening on http://localhost:${port}`);
     
+})
+})
+.catch((err)=>{
+    console.error("MongoDb connection error", err)
 })
 
  
